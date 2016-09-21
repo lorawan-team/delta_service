0f39c4f7ecdcbb6ec4e80cdc9931a8cb8d785cb7
Leander Molegraaf <leandermolegraaf@gmail.com>
2016-09-21 11:50:43 +0200

    Removed old database definitions as they are now in a seperate package

--- databases/migrations/2016_09_12_113249_create_measurements_table.php
+++ databases/migrations/2016_09_12_113249_create_measurements_table.php
@@ -3,35 +3,35 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateSensorDataTable extends Migration {
+class CreateMeasurementsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('migrations', function(Blueprint $table)
-		{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('measurements', function(Blueprint $table)
+        {
             $table->increments('id');
-			$table->integer('sensor_id')->unsigned()->index('fk_sensor_data_sensor1_idx');
-			$table->integer('value');
-			$table->dateTime('created_at');
-		});
-	}
+            $table->integer('sensor_id')->unsigned()->index('fk_measurements_sensor1_idx');
+            $table->integer('value');
+            $table->dateTime('created_at');
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
         DB::statement('SET FOREIGN_KEY_CHECKS = 0');
-		Schema::drop('migrations');
+        Schema::drop('measurements');
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
-	}
+    }
 
 }
--- databases/migrations/2016_09_12_113250_add_foreign_keys_to_measurements_table.php
+++ databases/migrations/2016_09_12_113250_add_foreign_keys_to_measurements_table.php
@@ -5,31 +5,31 @@ use Illuminate\Database\Schema\Blueprint;
 
 class AddForeignKeysToMeasurementsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('measurements', function(Blueprint $table)
-		{
-			$table->foreign('sensor_id', 'fk_measurements_sensor1')->references('id')->on('sensor')->onUpdate('NO ACTION')->onDelete('NO ACTION');
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('measurements', function(Blueprint $table)
+        {
+            $table->foreign('sensor_id', 'fk_measurements_sensor1')->references('id')->on('sensor')->onUpdate('NO ACTION')->onDelete('NO ACTION');
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('measurements', function(Blueprint $table)
-		{
-			$table->dropForeign('fk_measurements_sensor1');
-		});
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('measurements', function(Blueprint $table)
+        {
+            $table->dropForeign('fk_measurements_sensor1');
+        });
+    }
 
 }
